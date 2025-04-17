4d38128667ea12a17bd394508f3d1ffb047d5192
KhaledAlaa643 <khaledalaaka643@gmail.com>
2025-04-17 17:03:40 +0200

    remove unused imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { NgScrollbarModule } from 'ngx-scrollbar';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
